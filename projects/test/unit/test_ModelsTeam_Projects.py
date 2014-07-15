483a2bce9ed589d50c80e207443f9fc952ab51b8
dbate <daniel.bate@irisys.co.uk>
2014-07-15 08:33:50 +0100

    Added tests for Team.projects

---
+++ projects/test/unit/test_ModelsTeam_Projects.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from projects.test.test_models import test_team, test_project
+
+
+class ModelsTeam_Projects(TestCase):
+    def test_TeamHasNoProjects_ProjectsIsEmpty(self):
+        t = test_team()
+
+        self.assertEqual(0, len(t.projects))
+
+    def test_TeamHasOneProject_ProjectsIsOnlyContainsThatItem(self):
+        t = test_team()
+        p = test_project(team=t)
+
+        self.assertEqual([p], list(t.projects))
+
+    def test_MultipleProjectsTeamHasOneProject_ProjectsIsOnlyContainsThatItem(self):
+        t = test_team()
+        p = test_project(team=t)
+
+        t2 = test_team(name="foo")
+        test_project(name="foo proj", team=t2)
+
+        self.assertEqual([p], list(t.projects))
+
+    def test_TeamHasMultipleProjects_ProjectsContainsAllProjects(self):
+        t = test_team()
+        p1 = test_project(team=t)
+        p2 = test_project(name="foo proj", team=t)
+
+        self.assertEqual({p1, p2}, list(t.projects))
